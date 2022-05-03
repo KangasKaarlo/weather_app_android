dedde57293159ca191cde47fe4d669e493f0398c
Kaarlo Kangas <kaarlo.kangas@gmail.com>
2022-05-03 10:10:48 +0300

    Add background and a very basic layout to the Hourly forecast

--- app/src/main/java/fi/tuni/tamk/tiko/weatherappcompose/Hourly.kt
+++ app/src/main/java/fi/tuni/tamk/tiko/weatherappcompose/Hourly.kt
@@ -1,9 +1,71 @@
 package fi.tuni.tamk.tiko.weatherappcompose
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Hourly() {
-    Text(text = "Hello World")
+    Box(
+        Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .background(
+                Brush.verticalGradient(
+                    listOf(
+                        Color(0xFFCEFFFC),
+                        Color(0xFF86A8E7),
+                        Color(0xFFD16BA5),
+                    )
+                )
+            )
+    )
+    Column(
+        modifier = Modifier
+            .padding(20.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Surface(
+            modifier = Modifier
+                .width(350.dp)
+                .height(700.dp)
+                .padding(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            color = Color.White.copy(alpha = 0.1f),
+            border = BorderStroke(1.dp, Color.White.copy(alpha = 0.8f))
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(20.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Today",
+                    color = Color.White,
+                    style = MaterialTheme.typography.h4
+                    )
+
+            }
+
+        }
+    }
+}
+@Composable
+@Preview(showSystemUi = true)
+fun Preview() {
+    Hourly()
 }
